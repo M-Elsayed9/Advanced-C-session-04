694a1f8bcbdb2ac4aba259502f51b709ce5e08e3
Mohamed Elsayed <78699419+M-Elsayed9@users.noreply.github.com>
2024-10-14 22:56:06 +0300

    Assignment solution

---
+++ Assignment/SalesPerson.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    public class SalesPerson : Employee
+    {
+        public int AchievedTarget { get; set; }
+        public bool CheckTarget(int Quota)
+        {
+            if (Quota < AchievedTarget)
+            {
+                OnEmployeeLayOff(new EmployeeLayOffEventArgs() { Cause = LayOffCause.Target });
+                return false;
+            }
+            return true;
+        }
+        protected override void OnEmployeeLayOff(EmployeeLayOffEventArgs e)
+        {
+            if (e.Cause == LayOffCause.Target || e.Cause == LayOffCause.Age)
+            {
+                base.OnEmployeeLayOff(e);
+            }
+        }
+    }
+}
